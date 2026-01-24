bca8be3dc21f278dab5ea51d3f158f1fa08eb57d
Robin Kai <13338318081@163.com>
2026-01-24 23:33:28 +0800

    map & reduce

---
+++ Git test/py projects/learn8-1.py
@@ -0,0 +1,28 @@
+#map&reduce
+# def normalize(name):
+#     name = name.lower()
+#     name = name[0].upper() + name[1:] //str是不可变类型
+#     return name
+#//仅首字母大写，等于name.capitalize()
+# # 测试:
+# L1 = ['adam', 'LISA', 'barT']
+# L2 = list(map(normalize, L1))
+# print(L2)
+#--------------------------------------------------------
+# from functools import reduce
+# def plus(x,y):
+#     return x*y
+#
+# def prod(L):
+#     return reduce(plus,L)
+#--------------------------------------------------------
+# from functools import reduce
+# digits = {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
+# def str1num(ch):
+#     return digits[ch]
+#
+# def str2float(s):
+#     int_parts,float_parts = s.split('.')
+#     integer = reduce(lambda x,y : x*10+y,map(str1num,int_parts))
+#     floater = reduce(lambda x, y: x * 10 + y, map(str1num, float_parts))
+#     return integer + floater / (10**len(float_parts))
