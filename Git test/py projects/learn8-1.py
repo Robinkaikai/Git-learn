f7d6f3bf02591740853e325b7e502b0326d906a9
Robin Kai <13338318081@163.com>
2026-01-26 17:31:50 +0800

    filter & sorted function

--- Git test/py projects/learn8-1.py
+++ Git test/py projects/learn8-1.py
@@ -26,3 +26,25 @@
 #     integer = reduce(lambda x,y : x*10+y,map(str1num,int_parts))
 #     floater = reduce(lambda x, y: x * 10 + y, map(str1num, float_parts))
 #     return integer + floater / (10**len(float_parts))
+#===========================================================================
+#filter
+# def is_palindrome(n):
+#     n = str(n)
+#     return n==n[::-1] !!!!!!
+#===================================
+#sorted
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+#
+# def by_name(t):
+#     return t[0]
+#
+# L2 = sorted(L, key=by_name)
+# print(L2)
+#---------------------------------------------
+# def by_score(t):
+#     return t[1]
+#
+# L2 = sorted(L, key=by_score,reverse=True)
+# print(L2)
+# lst = [0,1,2,3,4]
+# print(list(reversed(lst)))
