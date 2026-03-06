2713737cc476bfd0095a7afd2db3d07b238f1f38
Robin Kai <13338318081@163.com>
2026-03-06 21:55:19 +0800

    enum

---
+++ Git test/py projects/15.py
@@ -0,0 +1,14 @@
+import re
+
+def is_valid_email(addr):
+    if re.match(r'^[\w.]*\@\w+\.\w+',addr):
+        return True
+    else:
+        return False
+
+# 测试:
+assert is_valid_email('someone@gmail.com')
+assert is_valid_email('bill.gates@microsoft.com')
+assert not is_valid_email('bob#example.com')
+assert not is_valid_email('mr-bob@example.com')
+print('ok')
